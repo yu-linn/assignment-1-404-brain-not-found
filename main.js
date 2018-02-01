9ce2087070f89f5280e3c78c06f477d5b9f9e946
Ambarish Gugilla <a.gugilla@gmail.com>
2018-01-31 19:52:43 -0500

    started getCollection() for News API

--- main.js
+++ main.js
@@ -3,7 +3,25 @@ const request = require('request')
 
 
 function getCollection(callback) {
-  // implement
+
+	//NEWS API
+	//Gets the JSON collection of top Canada headlines
+	let  query = "https://newsapi.org/v2/top-headlines?country=ca&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77"
+
+	request.get(query, function(error, response, body){
+		if(error){
+			return error
+		}
+
+		if(response.statusCode === 200){
+			let fullData = JSON.parse(body);
+			console.log(fullData)
+			callback(fullData.articles)
+		}
+
+	});
+
+
 }
 
 function getSortedCollection(callback) {
